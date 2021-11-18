e8c7994f3070a7036ff9d11bec0ec4ba85b27dfa
acaldwell1986 <acaldwell1986@gmail.com>
2021-11-18 16:19:51 -0500

    Created a switch test file

---
+++ switchtest.js
@@ -0,0 +1,20 @@
+let weekDay = prompt("What day of the week is it?")
+
+
+switch (weekDay) {
+    case 'Monday': alert("Ugh. I hate mondays.")
+    break;
+    case 'Tuesday': alert("Woo! Tacos.")
+    break;
+    case 'Wednesday': alert("Halfway done the week!")
+    break;
+    case 'Thursday': alert("Throwbacks if you must.")
+    break;
+    case 'Friday': alert("We're done for the week!!")
+    break;
+    case 'Saturday': alert("Time for chores and then resting.")
+    break;
+    case 'Sunday': alert("Watching football!")
+    break;
+    default: alert("Please enter a valid day, my dude.")
+}

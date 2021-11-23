c91500bae1cbf72655ef059d043a06a03684dad8
acaldwell1986 <acaldwell1986@gmail.com>
2021-11-23 16:23:29 -0500

    finished function practice from TOP

---
+++ lastLetter.js
@@ -0,0 +1,16 @@
+let myString = prompt(`What is my string?`)
+
+
+function lastLetter(funcString) {
+
+    let length = funcString.length - 1
+
+    return funcString.substr(length)
+}
+
+console.log(lastLetter(myString))
+
+
+
+
+

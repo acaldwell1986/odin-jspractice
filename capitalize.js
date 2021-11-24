55f0f46203da88277c8a031f2b7128157115db54
acaldwell1986 <acaldwell1986@gmail.com>
2021-11-24 15:39:53 -0500

    fixed capitalize function

--- capitalize.js
+++ capitalize.js
@@ -1,10 +1,15 @@
 let myString = prompt('What is my string?')
 
-lowerString = myString.toLowerCase()
-lowerLetters = lowerString.substr(1)
-upperLetter = myString.substr(0,1)
 
-console.log(lowerLetters)
-console.log(upperLetter)
 
-alert(upperLetter.toUpperCase() + lowerLetters)
+
+function stringCap(someString) {
+    let lowerString = someString.toLowerCase()
+    let lowerLetters = lowerString.substr(1)
+    let upperLetter = someString.substr(0,1)
+    return (upperLetter.toUpperCase() + lowerLetters)
+
+}
+
+
+console.log(stringCap(myString))

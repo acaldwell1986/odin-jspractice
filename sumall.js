9d24272b231abcec360c16c81e558f3f9b289dd6
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-08 15:14:17 -0500

    Created sumAll function for TOP

---
+++ sumall.js
@@ -0,0 +1,11 @@
+const sumAll = function(start, end) {
+    start = 0;
+    let sum = 0;
+       while (start !== end) {
+         start += 1;
+         sum+=start;
+       }
+   return sum;    
+   };
+   
+ 

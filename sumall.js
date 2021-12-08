891bacdbaf528441155d32d22bc4b176a8e1050f
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-08 15:44:17 -0500

    Added error handling to sumall function

--- sumall.js
+++ sumall.js
@@ -1,11 +1,17 @@
 const sumAll = function(start, end) {
-    start = 0;
-    let sum = 0;
-       while (start !== end) {
-         start += 1;
-         sum+=start;
-       }
-   return sum;    
-   };
+    let sum = 1;
+    if (Number.isInteger(end)) {
+        if (start > end) {
+            let temp = start; 
+            start = end; 
+            end = temp; 
+        };
+        while (start !== end) {
+            start += 1;
+            sum+=start;
+        };
+        return sum; 
+    } else 
+        return 'ERROR' 
+};
    
- 

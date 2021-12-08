33f81daa5350c3658dd4472914af060f40c3d459
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-08 14:17:28 -0500

    Fixed function so it takes multiple arguments and converts them into an array

--- removefromarray.js
+++ removefromarray.js
@@ -1,16 +1,29 @@
-const removeFromArray = function(stringArray, ...num) {
 
+    const removeFromArray = function() {
+        let stringArray = arguments[0];
+        let args = Array.from(arguments);
+        console.log(args);
+        args.shift(); //remove the array to be compared from list of args
+        console.log(args);
+        console.log(stringArray);
+        args.length;
 
-    for (let i = 0; i < stringArray.length; i++) {
+         
+        for (let i = 0; i < stringArray.length; i++) {
 
-        if (stringArray[i] == num[i]) {
-            console.log(`${typeof num[i]}`)
-            stringArray.splice(i,1);
-            
-        } 
+            console.log(i + 'outer');
 
-    }
-};
-let example = ['Hey','Hey2','Hey3','Hey4','Hey5'];
-
-console.log(removeFromArray(example, "Hey4", "Hey3"))
+            for (let k = 0; k < args.length; k++) {
+                console.log(k);
+                if (args[k] === stringArray[i]) {
+                    stringArray.splice(i,1);
+                    console.log(stringArray);
+                }
+            }
+        }
+        return stringArray;
+        
+        }; 
+        let example = ['Hey','Hey2','Hey3','Hey4','Hey5'];
+          
+        console.log(removeFromArray(example, "Hey3", "Hey4"));

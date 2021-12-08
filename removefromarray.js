52bd233c87a7a5c9d638f3954a77dadb99bf03f9
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-08 14:27:46 -0500

    Fixed outer for loop

--- removefromarray.js
+++ removefromarray.js
@@ -3,27 +3,23 @@
         let stringArray = arguments[0];
         let args = Array.from(arguments);
         console.log(args);
+        console.log(stringArray);
         args.shift(); //remove the array to be compared from list of args
         console.log(args);
         console.log(stringArray);
-        args.length;
 
          
         for (let i = 0; i < stringArray.length; i++) {
-
-            console.log(i + 'outer');
-
             for (let k = 0; k < args.length; k++) {
-                console.log(k);
                 if (args[k] === stringArray[i]) {
                     stringArray.splice(i,1);
-                    console.log(stringArray);
+                    i--;
                 }
             }
         }
         return stringArray;
         
         }; 
-        let example = ['Hey','Hey2','Hey3','Hey4','Hey5'];
+        let example = [1,2,3,4];
           
-        console.log(removeFromArray(example, "Hey3", "Hey4"));
+        console.log(removeFromArray(example, 3, 2));
